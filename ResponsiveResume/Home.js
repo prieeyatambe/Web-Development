c72de12fa4bce4947f093c3c2e66d464d8e3f241
prieeyatambe <priyatambe2206@gmail.com>
2024-09-03 15:31:16 +0530

    Second Commit

--- ResponsiveResume/Home.js
+++ ResponsiveResume/Home.js
@@ -1,12 +1,26 @@
 const uniInfoButton=document.getElementById('displayUniversity');
 const uniInfo=document.getElementById('universityInfo');
 
-uniInfoButton.addEventListener('click',()=>{
+uniInfoButton.addEventListener('mouseover',()=>{
     {
         if(uniInfo.style.display==='none' || uniInfo.style.display===''){
             uniInfo.style.display='block';
         }else{
             uniInfo.style.display='none';
+        }
+        
+    }
+})
+
+const courseButton =document.getElementById('displayCourse');
+const courseInfo=document.getElementById('courseInfo');
+
+courseButton.addEventListener('mouseover',()=>{
+    {
+        if(courseInfo.style.display==='none' || courseInfo.style.display===''){
+            courseInfo.style.display='block';
+        }else{
+            courseInfo.style.display='none';
         }
         
     }
